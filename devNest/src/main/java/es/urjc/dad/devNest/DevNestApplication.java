c58c87d3d72fa9e5197a64e64121a22cd95021da
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-02-01 17:55:23 +0100

    Project created
    devNest Spring project created with all needed dependencies. JPA dependency commented until the database is added

---
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.devNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevNestApplication.class, args);
+	}
+
+}
---
+++ devNest/src/test/java/es/urjc/dad/devNest/DevNestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.devNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
