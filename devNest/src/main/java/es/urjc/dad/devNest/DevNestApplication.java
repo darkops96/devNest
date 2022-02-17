fd670c9122a832830819efe2ef9a9c74597f11e2
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-02-17 18:14:53 +0100

    Delete unnecessary import from Application class

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
@@ -3,8 +3,6 @@ package es.urjc.dad.devNest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.urjc.dad.devNest.Internal_Services.RandomWord;
-
 @SpringBootApplication
 public class DevNestApplication {
 
