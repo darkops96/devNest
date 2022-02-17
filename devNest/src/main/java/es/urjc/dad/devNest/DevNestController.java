505d991e4dd37252d720af3de824ec6b41aa3bff
chamix0 <charlie.chamizo@gmail.com>
2022-02-17 22:29:18 +0100

    random words + buttons take you to places

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
@@ -1,20 +1,25 @@
 package es.urjc.dad.devNest;
 
+import es.urjc.dad.devNest.Internal_Services.RandomWord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class DevNestController
-{
-    
+public class DevNestController {
+
+    @Autowired
+    RandomWord randomWord;
+
+    //region INITIAL WEB
     @GetMapping("/")
-    public String home(Model model){
-        
+    public String home(Model model) {
+
         //Random generator
-        model.addAttribute("topic1", "Oscuridad");
-        model.addAttribute("topic2", "Saltos");
+        model.addAttribute("topic1", randomWord.getRandomWord());
+        model.addAttribute("topic2", randomWord.getRandomWord());
 
         //GameJams table
         model.addAttribute("name", "GameGen GameJam");
@@ -26,16 +31,16 @@ public class DevNestController
 
         return "initialWeb";
     }
+    //endregion
+
 
     @GetMapping("/login")
-    public String goToLogin(Model model)
-    {
+    public String goToLogin(Model model) {
         return "loginWeb";
     }
 
     @GetMapping("/register")
-    public String goToRegister(Model model)
-    {
+    public String goToRegister(Model model) {
         return "registerWeb";
     }
 }
