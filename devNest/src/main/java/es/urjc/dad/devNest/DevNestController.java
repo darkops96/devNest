87ce0c8d4ecbfeec8c37f6ac68ca0c2bc6cc3cae
Pablo de la Hoz Menéndez <79284028+darkops96@users.noreply.github.com>
2022-02-17 21:52:49 +0100

    Database controller connection (#11)
    * User service (user repository wired to controller)
    
    * Register and Log In params fixed

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
@@ -1,13 +1,23 @@
 package es.urjc.dad.devNest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.urjc.dad.devNest.Internal_Services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class DevNestController
 {
+
+    @Autowired
+    private UserService userService;
     
     @GetMapping("/")
     public String home(Model model){
@@ -37,5 +47,40 @@ public class DevNestController
     public String goToRegister(Model model)
     {
         return "registerWeb";
+    }
+
+    @RequestMapping(value="/loginUser", method = RequestMethod.POST, params={"username", "password"})
+    public String login(@RequestParam String username, @RequestParam String password)
+    {
+        boolean result = userService.login(username, password);
+        if(result)
+        {
+            return "redirect:/initialWeb";
+        }
+        else
+        {
+            return "redirect:/login";
+        } 
+    }
+
+    @RequestMapping(value="/registerUser", method = RequestMethod.POST, params={"email", "username", "password"})
+    public String register(@RequestParam String username, @RequestParam String password, @RequestParam String email)
+    {
+        boolean result = userService.register(username, password, email);
+        if(result)
+        {
+            return "redirect:/initialWeb";
+        }
+        else
+        {
+            return "redirect:/register";
+        }        
+    }
+
+    @GetMapping("/logout")
+    public String logout(Model model)
+    {
+        userService.logout();
+        return "initialWeb";
     }
 }
