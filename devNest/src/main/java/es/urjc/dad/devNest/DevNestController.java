a587caa8d508f77946e1936567d22aebb5e613ad
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-02-18 18:38:16 +0100

    Request method dependency deleted

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import es.urjc.dad.devNest.Internal_Services.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
