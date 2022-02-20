8bb973a178a435cfdc08873396e4ce7b58454852
chamix0 <charlie.chamizo@gmail.com>
2022-02-20 18:18:27 +0100

    game service

---
+++ devNest/src/main/java/es/urjc/dad/devNest/Internal_Services/GameService.java
@@ -0,0 +1,18 @@
+package es.urjc.dad.devNest.Internal_Services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class GameService {
+
+
+
+    //add a new game
+
+    //get a game
+
+    //
+
+}
