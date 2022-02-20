15940d47b903db86b24a6ed4cfe033c01dbb267c
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-02-20 14:05:26 +0100

    Delete unnecessary import

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestController.java
@@ -4,7 +4,6 @@ import es.urjc.dad.devNest.Database.Entities.UserEntity;
 import es.urjc.dad.devNest.Internal_Services.GameJamService;
 import es.urjc.dad.devNest.Internal_Services.RandomWord;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
