3c7760edc8fefb5b7673f23db13d18af918db841
juanjomalouu <abadjuanjo@hotmail.com>
2022-03-01 14:11:36 +0100

    Project Created

---
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/DevNestInternalServiceApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.devNestInternalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevNestInternalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevNestInternalServiceApplication.class, args);
+	}
+
+}
---
+++ devNestInternalService/src/test/java/es/urjc/dad/devNestInternalService/DevNestInternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.devNestInternalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNestInternalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
