bfe64e9b92a59ac83c45e84aa979f9a8ce109a7a
juanjomalouu <abadjuanjo@hotmail.com>
2022-03-15 13:38:04 +0100

    Internal Controllers & Services generated

---
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Controllers/FileController.java
@@ -0,0 +1,8 @@
+package es.urjc.dad.devNestInternalService.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileController {
+    
+}
---
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Internal_Services/FileService.java
@@ -0,0 +1,5 @@
+package es.urjc.dad.devNestInternalService.Internal_Services;
+
+public class FileService {
+    
+}
