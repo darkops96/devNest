c36e77b1400553df361cda4ed5984690fc5f6362
juanjomalouu <abadjuanjo@hotmail.com>
2022-03-19 17:18:25 +0100

    Cambios en File Controller

--- devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Controllers/FileController.java
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Controllers/FileController.java
@@ -1,8 +1,31 @@
 package es.urjc.dad.devNestInternalService.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.urjc.dad.devNestInternalService.Internal_Services.FileService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 public class FileController {
     
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping("/download-videogame/{id}")
+    public ResponseEntity<String> getVideogame(@PathVariable long id){
+        if(fileService.download(id)){
+            
+        }
+        else{
+
+        }
+        
+        
+        return ResponseEntity.ok("Found File");
+    }
 }
--- devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Internal_Services/FileService.java
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Internal_Services/FileService.java
@@ -1,5 +1,29 @@
 package es.urjc.dad.devNestInternalService.Internal_Services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.urjc.dad.devNestInternalService.Database.Entities.VideogameEntity;
+import es.urjc.dad.devNestInternalService.Database.Repositories.VideogameRepository;
+
+
+@Service
 public class FileService {
-    
+
+    @Autowired
+    private VideogameRepository videogameRepository;
+
+
+    public boolean download(long id){
+        Optional<VideogameEntity> videogame = videogameRepository.findById(id);
+        if(videogame.isPresent()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
 }
