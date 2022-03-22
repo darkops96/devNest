4983b97fdc317d78aa5b8062327e2ce8c97e3dd2
Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>
2022-03-22 19:36:33 +0100

    Cambios en FileController y FileService

--- devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Controllers/FileController.java
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Controllers/FileController.java
@@ -1,12 +1,23 @@
-package es.urjc.dad.devNestInternalService.Controllers;
+import java.io.IOException;
+import java.sql.Blob;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.Document;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.urjc.dad.devNestInternalService.Internal_Services.FileService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -17,15 +28,33 @@ public class FileController {
     private FileService fileService;
 
     @GetMapping("/download-videogame/{id}")
-    public ResponseEntity<String> getVideogame(@PathVariable long id){
-        if(fileService.download(id)){
-            
+    public ResponseEntity getVideogame(@PathVariable long id) throws Exception
+    {
+        if(fileService.isDownloable(id)){
+            return fileService.download(id);
         }
-        else{
-
+        else{       
+            return (ResponseEntity) ResponseEntity.notFound();
         }
-        
-        
-        return ResponseEntity.ok("Found File");
     }
+    
+    /*
+    @GetMapping(value = "/zip-download", produces="application/zip")
+    public void zipDownload(@RequestParam String filePath, HttpServletResponse response) throws IOException{
+        ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream());
+        //Esto sirve para meter el archivo con filePath en un ZIP aunque ya debería ser un ZIP por lo que sobraría
+            FileSystemResource resource = new FileSystemResource(filePath);
+            ZipEntry zipEntry = new ZipEntry(resource.getFilename());
+            zipEntry.setSize((resource.contentLength()));
+            zipOut.putNextEntry(zipEntry);
+            StreamUtils.copy(resource.getInputStream(),zipOut);
+            zipOut.closeEntry();
+            zipOut.finish();
+            zipOut.close();
+        ///////////
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + filePath + "\"");
+    }
+*/
+  
 }
--- devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Internal_Services/FileService.java
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/Internal_Services/FileService.java
@@ -1,9 +1,14 @@
-package es.urjc.dad.devNestInternalService.Internal_Services;
-
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Blob;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import es.urjc.dad.devNestInternalService.Database.Entities.VideogameEntity;
@@ -17,14 +22,36 @@ public class FileService {
     private VideogameRepository videogameRepository;
 
 
-    public boolean download(long id){
+    public ResponseEntity download(long id) throws Exception{
+        /* DESCARGAR UN ZIP*/
+        /*
         Optional<VideogameEntity> videogame = videogameRepository.findById(id);
-        if(videogame.isPresent()){
-            return true;
+        VideogameEntity vj = videogame.get();//Meter en el if
+        Blob file = vj.getGameFile();//Meter en el if
+        try{
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/zip"))
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+vj.getId()+"\"")
+            .body(new ByteArrayResource(file.getBytes(1, (int) file.length())));    
         }
-        else{
-            return false;
-        }
-
+        catch(Exception e)
+        {
+            throw new Exception("Error downloading file");
+        }   */
+        /* PARA COMPROBAR CON UN ARCHIVO DEL ORDENADOR: */
+        String filename = "D:/Archivos de Programa/Escritorio/devNest/devNestInternalService/Zip-to-delete/hola.zip";
+        File file = new File(filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires","0");
+        ResponseEntity<Object> responseEntity = ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(MediaType.parseMediaType("application/zip")).body(resource);
+        return responseEntity;
+    }
+    
+    public boolean isDownloable(long id){
+        Optional<VideogameEntity> videogame = videogameRepository.findById(id);
+        return videogame.isPresent();
     }
 }
