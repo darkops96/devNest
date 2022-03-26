ae2a754f1d1437072ba90ba848280fbc14e434f7
Pablo de la Hoz Menéndez <79284028+darkops96@users.noreply.github.com>
2022-03-26 12:15:00 +0100

    Prepare project for build (#59)
    - Set version
    - Delete Debug settings in application.properties
    - Add web icon

--- devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/DevNestInternalServiceApplication.java
+++ devNestInternalService/src/main/java/es/urjc/dad/devNestInternalService/DevNestInternalServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevNestInternalServiceApplication {
-
+public class DevNestInternalServiceApplication
+{
 	public static void main(String[] args)
 	{
 		String newLine = System.lineSeparator();
@@ -21,5 +21,4 @@ public class DevNestInternalServiceApplication {
 		
 		SpringApplication.run(DevNestInternalServiceApplication.class, args);
 	}
-
 }
