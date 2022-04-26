ac7ac5b149a14dbc2289c9e6dc96f925c22c7bd0
Pablo de la Hoz Menéndez <79284028+darkops96@users.noreply.github.com>
2022-04-26 18:59:45 +0200

    Add Game Jams cache (#83)
    * Add cache
    
    * Add Hazelcast log info
    
    * Add GameJams cache
    
    - Add anotations to GamejamRepository class
    - Implement equals and hashCode in GamejamEntity class

--- devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
@@ -5,14 +5,23 @@ import java.util.Collections;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 @SpringBootApplication
+@EnableCaching
 @EnableHazelcastHttpSession
 public class DevNestApplication {
+
+    private static final Log logger = LogFactory.getLog(DevNestApplication.class);
+
     public static void main(String[] args) {
         String newLine = System.lineSeparator();
         System.out.println(newLine +
@@ -29,8 +38,17 @@ public class DevNestApplication {
     }
 
     @Bean
+    public CacheManager cacheManager()
+    {
+        logger.info("Initializing cache...");
+        return new ConcurrentMapCacheManager("devnest");
+    }
+
+    @Bean
     public Config config()
     {
+        logger.info("Initializing Hazelcast...");
+        
         Config config = new Config();
         JoinConfig joinConfig = config.getNetworkConfig().getJoin();
 
