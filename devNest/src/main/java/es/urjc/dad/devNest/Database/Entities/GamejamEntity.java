61f19873d036c0921521df2af317a69dc5721eeb
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-04-29 18:56:29 +0200

    Update GamejamEntity.java
    Not Eager fetch
    
    Co-Authored-By: Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>

--- devNest/src/main/java/es/urjc/dad/devNest/Database/Entities/GamejamEntity.java
+++ devNest/src/main/java/es/urjc/dad/devNest/Database/Entities/GamejamEntity.java
@@ -34,7 +34,7 @@ public class GamejamEntity implements Serializable
     @Column(nullable = false)
     private String endDate;
 
-    @OneToMany(mappedBy = "gamejam", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "gamejam")
     @Fetch(FetchMode.SELECT)
     private List<TeamEntity> teams;
 
