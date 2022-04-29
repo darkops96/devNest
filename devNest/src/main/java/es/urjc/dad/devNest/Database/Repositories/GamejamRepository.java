363e654c1568e4dc0cd473539a27af63f604f493
Pablo (Darkops) <79284028+darkops96@users.noreply.github.com>
2022-04-29 14:43:20 +0200

    Update GamejamRepository.java

--- devNest/src/main/java/es/urjc/dad/devNest/Database/Repositories/GamejamRepository.java
+++ devNest/src/main/java/es/urjc/dad/devNest/Database/Repositories/GamejamRepository.java
@@ -5,9 +5,12 @@ import es.urjc.dad.devNest.Database.Entities.GamejamEntity;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface GamejamRepository extends JpaRepository<GamejamEntity,Long> {
     @Cacheable("gamejams")
@@ -16,6 +19,8 @@ public interface GamejamRepository extends JpaRepository<GamejamEntity,Long> {
     List<GamejamEntity> findAll();
 
     @Cacheable("gamejams")
+    @Override
+    @QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
     Optional<GamejamEntity> findById(Long id);
 
     @CacheEvict(value = "gamejams", allEntries = true)
