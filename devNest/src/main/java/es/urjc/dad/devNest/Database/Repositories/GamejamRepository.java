5f9f2539820b0a60abe619048687c49c60dd48ba
chamix0 <charlie.chamizo@gmail.com>
2022-05-04 18:42:12 +0200

    Cache final version

--- devNest/src/main/java/es/urjc/dad/devNest/Database/Repositories/GamejamRepository.java
+++ devNest/src/main/java/es/urjc/dad/devNest/Database/Repositories/GamejamRepository.java
@@ -8,24 +8,21 @@ import java.util.Optional;
 import javax.persistence.QueryHint;
 
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.QueryHints;
 
-public interface GamejamRepository extends JpaRepository<GamejamEntity,Long> {
-    @Cacheable("gamejams")
-    @Override
-    @QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
+public interface GamejamRepository extends JpaRepository<GamejamEntity, Long> {
+    @Cacheable(value = "gamejams", key = "#id")
     List<GamejamEntity> findAll();
 
-    @Cacheable("gamejams")
-    @Override
-    @QueryHints({@QueryHint(name = "org.hibernate.cacheable", value = "true")})
+    @Cacheable(value = "gamejams", key = "#id")
     Optional<GamejamEntity> findById(Long id);
 
-    @CacheEvict(value = "gamejams", allEntries = true)
+    @CachePut(value = "gamejams", key = "#gamejamEntity.id")
     GamejamEntity save(GamejamEntity gamejamEntity);
-    
-    @CacheEvict(value = "gamejams", allEntries = true)
+
+    @CacheEvict(value = "gamejams", key = "#entity.id")
     void delete(GamejamEntity entity);
 }
--- devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
+++ devNest/src/main/java/es/urjc/dad/devNest/DevNestApplication.java
@@ -26,48 +26,62 @@ public class DevNestApplication {
     public static void main(String[] args) {
         String newLine = System.lineSeparator();
         System.out.println(newLine +
-                           "*******************************" + newLine +
-                           "*********** DEVNEST ***********" + newLine +
-                           "*******************************" + newLine +
-                           "Developed by:" + newLine +
-                           "- Pablo de la Hoz Menendez" + newLine +
-                           "- Carlos Chamizo Cano" + newLine +
-                           "- Juan Jose Perez Abad" + newLine +
-                           "*******************************" + newLine);
+                "*******************************" + newLine +
+                "*********** DEVNEST ***********" + newLine +
+                "*******************************" + newLine +
+                "Developed by:" + newLine +
+                "- Pablo de la Hoz Menendez" + newLine +
+                "- Carlos Chamizo Cano" + newLine +
+                "- Juan Jose Perez Abad" + newLine +
+                "*******************************" + newLine);
 
         SpringApplication.run(DevNestApplication.class, args);
     }
 
     /**
-    * Method in charge of initializing the database cache
-    */
+     * Method in charge of initializing the database cache
+     *
+     * @param hazelcastInstance
+     * @return
+     */
     @Bean
-    public CacheManager cacheManager(HazelcastInstance hazelcastInstance)
-    {
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
         logger.info("Initializing Game Jams database cache");
         return new HazelcastCacheManager(hazelcastInstance);
     }
 
+    /**
+     * Get instance of hazelcast using custom configuration
+     *
+     * @param config
+     * @return
+     */
     @Bean
-    public HazelcastInstance hazelcastInstance (Config config){
+    public HazelcastInstance hazelcastInstance(Config config) {
         return Hazelcast.newHazelcastInstance(config);
     }
 
+
     /**
-    * Method in charge of initializing Hazelcast
-    */
+     * Method in charge of initializing Hazelcast
+     *
+     * @return
+     */
     @Bean
-    public Config config()
-    {
+    public Config config() {
         logger.info("Initializing Hazelcast");
-        
+
+        // Access Hazelcast configuration
         Config config = new Config();
         JoinConfig joinConfig = config.getNetworkConfig().getJoin();
 
+        // Activate multicast
         joinConfig.getMulticastConfig().setEnabled(true);
 
+        // Create cache
         MapConfig usersMapConfig = new MapConfig().setName("gamejams");
 
+        // Add cache to config
         config.addMapConfig(usersMapConfig);
 
         return config;
