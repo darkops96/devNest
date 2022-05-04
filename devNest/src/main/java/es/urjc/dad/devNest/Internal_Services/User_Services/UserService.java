48706d0dc2ea2e92d43756d0808bb0c257449424
Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>
2022-05-04 20:12:12 +0200

    Transactional added

--- devNest/src/main/java/es/urjc/dad/devNest/Internal_Services/User_Services/UserService.java
+++ devNest/src/main/java/es/urjc/dad/devNest/Internal_Services/User_Services/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -141,6 +142,7 @@ public class UserService {
      *
      * @param user user updated
      */
+    @Transactional
     public void updateUser(UserEntity user) {
         Session session = entityManager.unwrap(Session.class);
         session.merge(user);
